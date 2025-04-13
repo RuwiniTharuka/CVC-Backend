3cc9bf677154d8e800ebf99facd0fdf15d6d43d1
RuwiniTharuka <ruwininawarathna22>
2025-04-13 18:43:08 +0530

    dotenv implementation🔒

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
 //console.log("Hello World!");
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
 //import Student from './models/student.js';
 //import studentRouter from './routs/studentRouter.js';
 //import itemRouter from './routs/itemRouter.js';
-import userRouter from './routs/userRouter.js';
+import userRouter from "./routs/userRouter.js";
 //import jwt, { decode } from "jsonwebtoken";
-import productRouter from './routs/productRuter.js';
-import verifyJWT from './middleware/auth.js';
-import orderRouter from './routs/orderRouter.js';
+import productRouter from "./routs/productRuter.js";
+import verifyJWT from "./middleware/auth.js";
+import orderRouter from "./routs/orderRouter.js";
+import dotenv from "dotenv";
+dotenv.config();
 /*let*/
-const app=express();
-mongoose.connect("mongodb+srv://admin:123@cluster0.futbr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(
-    ()=>{
-        console.log("connected to the database");
-    }
-).catch(
-    ()=>{
-        console.log("connection failed");
-    }
-)
+const app = express();
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("connected to the database");
+  })
+  .catch(() => {
+    console.log("connection failed");
+  });
 app.use(bodyParser.json());
-app.use(verifyJWT)
+app.use(verifyJWT);
 /*embed auth.js app.use(
   (req,res,next)=>{
       const header=req.header("Authorization");
@@ -38,12 +39,12 @@ app.use(verifyJWT)
       }
      next()
   }
-)*/ 
+)*/
 
 // console.log(header);
-app.use("/api/user",userRouter)
-app.use("/api/product",productRouter)
-app.use("/api/order",orderRouter)
+app.use("/api/user", userRouter);
+app.use("/api/product", productRouter);
+app.use("/api/order", orderRouter);
 
 /*app.use("/api/student",studentRouter);
 app.use("/api/item",itemRouter);
@@ -51,99 +52,79 @@ app.listen(3000,()=>{
     console.log("server is running on port 3000");
 })
 */
-function taskComplete(){
-    console.log("Task Completed");
+function taskComplete() {
+  console.log("Task Completed");
 }
 //app.listen(3000,taskComplete)
 
-app.get("/",
-    (req,res)=>{        
-        Student.find().then(
-        (students)=>{
-            res.json(students)
-        }
-      ).catch(
-        ()=>{
-            res.json(
-                {
-                    message: "An error occurred"
-                }
-            )
-        }
-      )
-        })
+app.get("/", (req, res) => {
+  Student.find()
+    .then((students) => {
+      res.json(students);
+    })
+    .catch(() => {
+      res.json({
+        message: "An error occurred",
+      });
+    });
+});
 
 //get request
-app.get("/",
-    (req,res)=>{        
-       // console.log(req.body)undefined
-      console.log(req.body)  // request show akaraya
-        console.log("Get request received");
-        res.json({
-            message: "Hello World"
-        });
-    }
-)
-app.post("/",
-    (req,res)=>{
-        //saving to the student in to the database
-       /* console.log(req.body)
+app.get("/", (req, res) => {
+  // console.log(req.body)undefined
+  console.log(req.body); // request show akaraya
+  console.log("Get request received");
+  res.json({
+    message: "Hello World",
+  });
+});
+app.post("/", (req, res) => {
+  //saving to the student in to the database
+  /* console.log(req.body)
        const studentSchema = new mongoose.Schema({
         name : String,
         age : Number,
         city : String
        })
        const studentModel = mongoose.model("students",studentSchema)*/
-       const student = new Student(req.body);//variable:student==>>student model:Student
-       student.save().then(
-        ()=>{
-            res.json({
-                    message:"student saved successfully"
-                })
-        }
-       ).catch(
-        ()=>{
-            res.json(
-                {
-                    message: "student save failed"
-                }
-            )
-        }
-       )
-    }
-)
+  const student = new Student(req.body); //variable:student==>>student model:Student
+  student
+    .save()
+    .then(() => {
+      res.json({
+        message: "student saved successfully",
+      });
+    })
+    .catch(() => {
+      res.json({
+        message: "student save failed",
+      });
+    });
+});
 //post request
-app.post("/",
-    (req,res)=>{        
-      console.log(req.body)  // request show akaraya
-        console.log("post request received");
-        res.json({
-            message: "This is a post request"
-        });
-    }
-)
+app.post("/", (req, res) => {
+  console.log(req.body); // request show akaraya
+  console.log("post request received");
+  res.json({
+    message: "This is a post request",
+  });
+});
 //delete request
-app.delete("/",
-    (req,res)=>{        
-      console.log(req.body)  // request show akaraya
-        console.log("Delete request received");
-        res.json({
-            message: "This is a delete request"
-        });
-    }
-)
+app.delete("/", (req, res) => {
+  console.log(req.body); // request show akaraya
+  console.log("Delete request received");
+  res.json({
+    message: "This is a delete request",
+  });
+});
 //put request
-app.put("/",
-    (req,res)=>{        
-      console.log(req.body)  // request show akaraya
-        console.log("Put request received");
-        res.json({
-            message: "This is a put request"
-        });
-    }
-)
-app.listen(5000,
-    ()=>{
-        console.log("Server is running on port 5000");
-    }
-)
+app.put("/", (req, res) => {
+  console.log(req.body); // request show akaraya
+  console.log("Put request received");
+  res.json({
+    message: "This is a put request",
+  });
+});
+app.listen(5000, () => {
+  console.log("Server is running on port 5000");
+});
